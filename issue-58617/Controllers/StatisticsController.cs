aeb72f921548a9f333d40898512e0bdb9d89d8d8
Mike Kistler <mikekistler@microsoft.com>
2024-10-25 12:08:40 -0700

    Repro of issue 58617

---
+++ issue-58617/Controllers/StatisticsController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Issue_58617.Controllers;
+
+[ApiController]
+public class StatisticsController : ControllerBase
+{
+
+    // TestDto corresponds to component schema "TestDto2" with nullable: true
+
+    [HttpGet("Test")]
+    public TestDto Test()
+    {
+        return default!;
+    }
+
+    [HttpGet("Test2")]
+    public TestDto? Test2()
+    {
+        return null;
+    }
+
+    // TestDto corresponds to component schema "TestDto" (with nullable: false)
+
+    [HttpGet("Test3")]
+    public KeyValuePair<int, TestDto> Test3()
+    {
+        return default!;
+    }
+}
+
+public class TestDto
+{
+    public string x { get; set; } = default!;
+}
