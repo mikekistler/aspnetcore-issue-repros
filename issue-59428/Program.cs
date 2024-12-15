39991a62e8a12bc09e41ff367d32085b9096720c
Mike Kistler <mikekistler@microsoft.com>
2024-12-15 07:17:38 -0800

    Repro of issue 59428

--- issue-59428/Program.cs
+++ issue-59428/Program.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -38,4 +40,7 @@ app.Run();
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    [MaxLength(100)]
+    public string[]? Stuff { get; set; }
 }
