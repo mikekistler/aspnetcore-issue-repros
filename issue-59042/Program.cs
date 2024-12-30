ac6a284e28822de6ca207eedb8308b38bc2fb59b
Mike Kistler <mikekistler@microsoft.com>
2024-12-30 09:07:00 -0600

    Repro for issue 59042

--- issue-59042/Program.cs
+++ issue-59042/Program.cs
@@ -1,3 +1,7 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,28 +18,20 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+// Endpoints that define [FromForm] parameters will be generated with both
+// "multipart/form-data" and "application/x-www-form-urlencoded" content entries
+app.MapPost("/from-form",
+(
+    [FromForm][Description("Name")][Required][MaxLength(26)][RegularExpression(@"^[A-Za-z0-9-]*$")] string name,
+    [FromForm][Description("Age")][Range(0,100)] int age,
+    // The property name will be the same as the parameter name with no case-coercion
+    [FromForm][Description("Pascal-cased parameter")] int? Pascal,
+    // as with the other parameter binding attributes, FromForm supports a Name parameter to specify the form field name
+    [FromForm(Name = "other-name")] string? otherName
+) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    // Create a dynamic object to return the values
+    return TypedResults.Ok(new { name, age });
+});
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
